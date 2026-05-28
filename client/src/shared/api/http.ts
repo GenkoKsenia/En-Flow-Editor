ce2a42169d283e2fe514c3da989b44c8b21c6dcb
Dmitriy Dolgarev <dimakx85@mail.ru>
2026-05-28 12:35:58 +0800

    Updated dist

--- client/src/shared/api/http.ts
+++ client/src/shared/api/http.ts
@@ -2,8 +2,6 @@ import axios from 'axios'
 
 export const http = axios.create({
   baseURL: 'https://localhost:7018/api',
+  //baseURL: 'https://enplusflow.dev.enplus.digital/api',
   withCredentials: true,
 })
-
-
-
--- client/src/shared/api/realtime/createHubConnection.ts
+++ client/src/shared/api/realtime/createHubConnection.ts
@@ -1,6 +1,7 @@
 import { HubConnectionBuilder, LogLevel, type HubConnection } from '@microsoft/signalr'
 
 const REALTIME_BASE_URL = 'https://localhost:7018'
+//const REALTIME_BASE_URL = 'https://enplusflow.dev.enplus.digital'
 
 export function createHubConnection(path: string): HubConnection {
   return new HubConnectionBuilder()
